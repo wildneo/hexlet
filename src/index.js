aec770b7950bab25bd858c42b642f44e69bd2c36
Roman Oskolkov <wild_neo@mail.ru>
2019-06-10 17:22:13 +0500

    Clear index

--- src/index.js
+++
@@ -1,9 +0,0 @@
-
-const wordsCount = (words, stopWords) => {
-  return words.map(e => e.toLowerCase()).filter(e => !stopWords.includes(e));
-};
-
-const stopWords = ['and', 'or', 'a', 'the', ''];
-const words = ['HellO', 'h', 'And', 'heLlo', '', 'AND', 'DOG', 'oR', 'cat', 'HELLO', 'caT'];
-
-console.log(wordsCount(words, stopWords));
