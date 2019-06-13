abd115337fd962027e1fda8ae5b8785c22a53817
Roman Oskolkov <wild_neo@mail.ru>
2019-06-13 17:38:05 +0500

    Enumerable in progrress

--- src/Enumerable.js
+++ src/Enumerable.js
@@ -5,22 +5,35 @@ class Enumerable {
   }
 
   get length() {
-    if (!this.memo) {
-      this.toArray();
-    }
-    return this.memo.length;
+    return this.getMemorizedCollection().length;
   }
 
   build(fn) {
     return new Enumerable(this.collection.slice(), this.operations.concat(fn));
   }
 
+  getMemorizedCollection() {
+    if (!this.memo) {
+      this.memo = this.operations.reduce((coll, operation) => operation(coll), this.collection);
+    }
+    return this.memo;
+  }
+
   select(fn) {
     return this.build(coll => coll.map(fn));
   }
 
-  where(fn) {
-    return this.build(coll => coll.filter(fn));
+  where(...parameters) {
+    const chekParamType = (param) => {
+      if (typeof param === 'function') {
+        return param;
+      }
+      if (typeof param === 'object') {
+        const key = Object.keys(param);
+        return car => car[key] === param[key];
+      }
+    };
+    return parameters.reduce((acc, param) => acc.build(coll => coll.filter(chekParamType(param))), this);
   }
 
   orderBy(fn, direction = 'asc') {
@@ -32,11 +45,21 @@ class Enumerable {
   }
 
   toArray() {
-    if (!this.memo) {
-      this.memo = this.operations.reduce((coll, operation) => operation(coll), this.collection);
-    }
-    return this.memo.slice();
+    return this.getMemorizedCollection().slice();
   }
 }
 
 export default Enumerable;
+
+const cars = [
+  { brand: 'bmw', model: 'm5', year: 2014 },
+  { brand: 'bmw', model: 'm4', year: 2013 },
+  { brand: 'kia', model: 'sorento', year: 2014 },
+  { brand: 'kia', model: 'rio', year: 2010 },
+  { brand: 'kia', model: 'sportage', year: 2012 },
+];
+const coll = new Enumerable(cars);
+
+const result = coll.where(car => car.brand === 'kia', { year: 2010 }, car => car.model === 'rio');
+
+console.log(result.toArray());
--- src/__tests__/Enumerable.test.js
+++ src/__tests__/Enumerable.test.js
@@ -55,8 +55,29 @@ describe('HexletLinq', () => {
       .where(car => car.brand === 'kia')
       .where(car => car.year > 2011);
 
-    expect(result).toHaveLength(2);
     expect(result.toArray()).toEqual([cars[2], cars[4]]);
+
+    const result2 = coll.where({ brand: 'bmw' });
+    expect(result2.toArray()).toEqual([cars[0], cars[1], cars[5]]);
+
+    const result3 = coll.where({ brand: 'kia', model: 'sorento' });
+    expect(result3.toArray()).toEqual([cars[2]]);
+
+    const result4 = coll.where({ brand: 'kia' }, car => car.year < 2013);
+    expect(result4.toArray()).toEqual([cars[3], cars[4]]);
+
+    const result5 = coll.where({ year: 2009 });
+    expect(result5.toArray()).toEqual([]);
+
+    const result6 = coll.where({ brand: 'kia' }, { model: 'sorento' });
+    expect(result6.toArray()).toEqual([cars[2]]);
+  });
+
+  it('#where 2', () => {
+    const result = coll.where(car => car.year < 2014)
+      .where(car => car.brand === 'kia', car => car.year > 2011);
+
+    expect(result.toArray()).toEqual([cars[4]]);
   });
 
   it('#select', () => {
