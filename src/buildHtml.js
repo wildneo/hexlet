443396864e00c0e793e51ec8880b80c028558494
Roman Oskolkov <wild_neo@mail.ru>
2019-06-21 17:57:33 +0500

    build html uint complite

--- src/buildHtml.js
+++ src/buildHtml.js
@@ -1,54 +1,45 @@
 
-const iter = (node) => {
-  if (node[0] instanceof Array) {
-    return { type: 'tagList', data: node.map(iter) };
-  }
-  const name = node[0];
-  const data = node[1] instanceof Array || typeof node[1] === 'string' ? node[1] : node[2];
-  const processedData = data instanceof Array ? iter(data) : data;
-  const attr = node[2] || !(node[1] instanceof Array || typeof node[1] === 'string') ? node[1] : {};
+const propActions = [
+  {
+    name: 'data',
+    check: arg => typeof arg === 'string',
+  },
+  {
+    name: 'children',
+    check: arg => arg instanceof Array,
+  },
+  {
+    name: 'attributes',
+    check: arg => arg instanceof Object,
+  },
+];
 
-  return { type: 'tag', name, data: processedData || '', attr: attr || {} };
-};
+const checkArg = arg => propActions.find(({ check }) => check(arg)).name;
 
-const iterAst = (ast) => {
-  switch (ast.type) {
-    case 'tagList':
-      return `${ast.data.map(iterAst).join('')}`;
+const inlineAttrs = attributes => (
+  Object.keys(attributes)
+    .map(key => ` ${key}="${attributes[key]}"`)
+    .join('')
+);
 
-    case 'tag':
-      const attrLine = Object.keys(ast.attr).reduce((acc, key) => (
-        `${acc} ${key}="${ast.attr[key]}"`), '');
-      return `<${ast.name}${attrLine}>${iterAst(ast.data)}</${ast.name}>`;
+const buildHtml = (node) => {
+  const [first, ...rest] = node;
+  const root = {
+    name: first,
+    data: '',
+    attributes: {},
+    children: [],
+  };
+  const tag = rest
+    .reduce((acc, arg) => (
+      { ...acc, [checkArg(arg)]: arg }
+    ), root);
 
-    default:
-      return ast;
-  }
-}
-const buildHtml = (tree) => {
-  const astTree = iter(tree);
-  console.log(JSON.stringify(astTree, null, 2));
-  return iterAst(astTree);
+  return [
+    `<${tag.name}${inlineAttrs(tag.attributes)}>`,
+    `${tag.data}${tag.children.map(buildHtml).join('')}`,
+    `</${tag.name}>`,
+  ].join('');
 };
 
 export default buildHtml;
-
-
-const data = ['html', [
-  ['head', [
-    ['title', 'hello, hexlet!'],
-  ]],
-  ['body', { class: 'container' }, [
-    ['h1', { class: 'header' }, 'html builder example'],
-    ['div', [
-      ['span'],
-      ['span', { class: 'text', id: 'uniq-key' }],
-    ]],
-  ]],
-]];
-const test = buildHtml(data);
-console.log(test);
-// console.log(JSON.stringify(test, null, 2));
-
-// console.log(buildHtml(data));
-// console.log(assert(data[1][1]));
