f3aac5769ff456fce155b03ee725e3db22de9047
Roman Oskolkov <wild_neo@mail.ru>
2019-06-24 18:06:36 +0500

    flatten challenge complite

--- src/challenges/js_trees_flatten.js
+++ src/challenges/js_trees_flatten.js
@@ -1,5 +1,9 @@
-const flatten = () => {
-  // Do something...
-};
+const flatten = arr => (
+  arr.reduce((acc, elem) => (
+    Array.isArray(elem)
+      ? [...acc, ...flatten(elem)]
+      : [...acc, elem]
+  ), [])
+);
 
 export default flatten;
