bd2c760477c7dcee59b52cbc4e66210b44de90de
Roman Oskolkov <wild_neo@mail.ru>
2019-08-30 21:06:42 +0500

    js-dom tablist complete

--- src/js-dom/__tests__/tablist.test.js
+++ src/js-dom/__tests__/tablist.test.js
@@ -8,7 +8,7 @@ const fixuturesPath = path.join(__dirname, '__fixtures__');
 const getTree = () => html(document.body.innerHTML);
 
 beforeAll(() => {
-  const initHtml = fs.readFileSync(path.join(fixuturesPath, 'tablist_index.html')).toString();
+  const initHtml = fs.readFileSync(path.join(fixuturesPath, 'tablist.html')).toString();
   document.documentElement.innerHTML = initHtml;
   run();
 });
--- src/js-dom/tablist.js
+++ src/js-dom/tablist.js
@@ -1,7 +1,15 @@
 export default () => {
+  const handler = ({ target }) => {
+    const closestNavTabs = target.closest('.nav-tabs');
+
+    const active = closestNavTabs.querySelector('.active');
+    const targetPane = document.querySelector(target.hash);
+    const activePane = document.querySelector(active.hash);
+
+    const switcher = ({ classList }) => classList.toggle('active');
+    [target, targetPane, active, activePane].map(switcher);
+  };
+
   const tabs = document.querySelectorAll('[data-toggle="tab"]');
-  tabs.forEach(tab => tab.addEventListener('click', (event) => {
-    const closestNavTabs = event.target.closest('.nav-tabs');
-    const activTab = closestNavTabs.querySelector('.active');
-  }));
+  tabs.forEach(tab => tab.addEventListener('click', handler));
 };
